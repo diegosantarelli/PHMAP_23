1edddfd4a76843b11aec9ba30981a967aca9a067
SimoneRecinelli <simonerecinelli@gmail.com>
2025-02-17 16:30:02 +0100

    developed task2

--- task2/1st classifier/one_class_classifier.m
+++ task2/1st classifier/one_class_classifier.m
@@ -1,47 +1,43 @@
-training_data_task2;
+function [finalModel, falsi_positivi] = one_class_classifier(training_set_task2, test_set, k)
+    % Generazione delle feature utilizzando la tua funzione
+    [featureTable_t2_1st, ~] = feature_gen_t2_1st(training_set_task2);
+    [featureTable_test_t2, ~] = feature_gen_t2_1st(test_set);
 
-% Generazione delle feature utilizzando la tua funzione
-[featureTable_t2_1st, ~] = feature_gen_t2_1st(training_set_task2);
+    % Supponiamo che le tabelle delle feature siano state generate correttamente
+    X_train = featureTable_t2_1st{:, 3:end}; % Supponiamo che le prime due colonne siano MemberID e Task2
+    X_test = featureTable_test_t2{:, 3:end};
 
-%%
-% Supponiamo che featureTable_t2_1st sia stato generato correttamente
-X_train = featureTable_t2_1st{:, 3:end}; % Supponiamo che le prime due colonne siano MemberID e Task2
+    cv = cvpartition(size(X_train, 1), 'KFold', k);
+    kfold_results = zeros(size(X_train, 1), 1);
 
-% K-fold Cross Validation per i dati noti (Task2 = 4)
-cv = cvpartition(size(X_train, 1), 'KFold', 5);
-kfold_results = zeros(size(X_train, 1), 1);
+    % Addestramento Isolation Forest con IFOREST su tutto il dataset di training
+    finalModel = iforest(X_train, 'NumLearners', 100);
 
-for i = 1:cv.NumTestSets
-    trainIdx = cv.training(i);
-    testIdx = cv.test(i);
+    for i = 1:cv.NumTestSets
+        trainIdx = cv.training(i);
+        testIdx = cv.test(i);
 
-    % Addestramento OCSVM su k-1 fold
-    model = ocsvm(X_train, StandardizeData=true, KernelScale="auto");
+        % Predizione sul fold di test (usanomaly restituisce direttamente le etichette)
+        [isAnomaly, ~] = isanomaly(finalModel, X_train(testIdx, :));
+        kfold_results(testIdx) = isAnomaly; % 1 se anomalia, 0 se normale
+    end
 
-    % Predizione sul fold di test
-    score = anomalyScore(model, X_train(testIdx, :));
-    kfold_results(testIdx) = isanomaly(score);
+    % Verifica falsi positivi (dati noti classificati come anomaly)
+    falsi_positivi = sum(kfold_results == 1);
+    disp(['Falsi positivi sui dati noti: ', num2str(falsi_positivi)]);
 
-
+    % Predizione sul test set esterno
+    [isAnomaly_test, ~] = isanomaly(finalModel, X_test);
+    disp(['Anomalie rilevate nel test set: ', num2str(sum(isAnomaly_test == 1))]);
 end
 
-% Verifica falsi positivi (dati noti classificati come anomaly)
-falsi_positivi = sum(kfold_results == 1);
-disp(['Falsi positivi sui dati noti: ', num2str(falsi_positivi)]);
-
-% %%
+%%
 % % Generazione di dati di rumore bianco
-% numSamples = 50; % Numero di campioni di rumore bianco
+% numSamples = 500; % Numero di campioni di rumore bianco
 % numFeatures = size(X_train, 2);
 % X_noise = randn(numSamples, numFeatures);
 % 
-% % Addestramento modello finale su tutto il training set
-% ocsvmModel = fitcsvm(X_train, 'KernelFunction', 'rbf', 'Standardize', true, 'Nu', 0.5);
-% ocsvmModel = fitSVMPosterior(ocsvmModel);
-% 
-% % Predizione sul rumore bianco
-% [~, score_noise] = predict(ocsvmModel, X_noise);
-% pred_noise = isanomaly(score_noise(:, 1));
-% anomalie_rumore_bianco = sum(pred_noise == 1);
+% % Predizione sul rumore bianco usando lo stesso modello
+% [isAnomaly_noise, ~] = isanomaly(finalModel, X_noise);
+% anomalie_rumore_bianco = sum(isAnomaly_noise == 1);
 % disp(['Anomalie rilevate nel rumore bianco: ', num2str(anomalie_rumore_bianco)]);
-
