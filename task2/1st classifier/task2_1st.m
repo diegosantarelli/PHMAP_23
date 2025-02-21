5457fe3aaa7712efa45ee632ee1cea99e8e62d65
diegosantarelli <s1118746@studenti.univpm.it>
2025-02-21 10:51:54 +0100

    minor changes

--- task2/1st classifier/task2_1st.m
+++ task2/1st classifier/task2_1st.m
@@ -1,5 +1,5 @@
-%import_data;
-%task1_completed;
+import_data;
+task1_completed;
 
 training_set_task2 = labeledData(labeledData.Task2 == 2 | labeledData.Task2 == 3, {'Case', 'Task2'});
 training_set_task2.Task2(:) = 4;
